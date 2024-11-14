8e1b111dbab958288bd22ae77e41517d41f70a12
4132431983 <nosratistudy2026@gmail.com>
2024-11-14 16:36:29 +0330

    Add files via upload

--- usdt_transfer.js
+++ usdt_transfer.js
@@ -12,25 +12,20 @@ const {
 } = process.env;
 
 // Initialize Web3
-const web3 = new Web3("https://eth-mainnet.alchemyapi.io/v2/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn");
+const web3 = new Web3(ALCHEMY_API_URL);
+
+// Validate address inputs
+if (!web3.utils.isAddress(SENDER_ADDRESS) || !web3.utils.isAddress(DESTINATION_ADDRESS) || !web3.utils.isAddress(USDT_CONTRACT_ADDRESS)) {
+  throw new Error("One or more Ethereum addresses are invalid. Check SENDER_ADDRESS, DESTINATION_ADDRESS, and USDT_CONTRACT_ADDRESS in your .env file.");
+}
 
 // USDT ERC-20 ABI (simplified for transfer)
 const USDT_ABI = [
   {
     "constant": true,
-    "inputs": [
-      {
-        "name": "_owner",
-        "type": "address"
-      }
-    ],
+    "inputs": [{ "name": "_owner", "type": "address" }],
     "name": "balanceOf",
-    "outputs": [
-      {
-        "name": "balance",
-        "type": "uint256"
-      }
-    ],
+    "outputs": [{ "name": "balance", "type": "uint256" }],
     "payable": false,
     "stateMutability": "view",
     "type": "function"
@@ -38,22 +33,11 @@ const USDT_ABI = [
   {
     "constant": false,
     "inputs": [
-      {
-        "name": "_to",
-        "type": "address"
-      },
-      {
-        "name": "_value",
-        "type": "uint256"
-      }
+      { "name": "_to", "type": "address" },
+      { "name": "_value", "type": "uint256" }
     ],
     "name": "transfer",
-    "outputs": [
-      {
-        "name": "",
-        "type": "bool"
-      }
-    ],
+    "outputs": [{ "name": "", "type": "bool" }],
     "payable": false,
     "stateMutability": "nonpayable",
     "type": "function"
@@ -61,7 +45,7 @@ const USDT_ABI = [
 ];
 
 // Set the USDT contract
-const usdtContract = new web3.eth.Contract(USDT_ABI, 0xdac17f958d2ee523a2206206994597c13d831ec7);
+const usdtContract = new web3.eth.Contract(USDT_ABI, USDT_CONTRACT_ADDRESS);
 
 // Function to send USDT
 async function sendUSDT() {
@@ -70,24 +54,24 @@ async function sendUSDT() {
     const gasPrice = await web3.eth.getGasPrice();
 
     // Convert amount to send from USDT (6 decimals) to wei
-    const amountInWei = web3.utils.toWei(2300, 'mwei'); // mwei = 6 decimals for USDT
+    const amountInWei = web3.utils.toWei(AMOUNT_TO_SEND, 'mwei'); // mwei = 6 decimals for USDT
 
     // Estimate gas for the transaction
-    const gasLimit = await usdtContract.methods.transfer(0x551510dFb352bf6C0fCC50bA7Fe94cB1d2182654, amountInWei).estimateGas({
-      from: 0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3,
+    const gasLimit = await usdtContract.methods.transfer(DESTINATION_ADDRESS, amountInWei).estimateGas({
+      from: SENDER_ADDRESS,
     });
 
     // Create the transaction
     const tx = {
-      from: 0x4DE23f3f0Fb3318287378AdbdE030cf61714b2f3,
-      to: 0xdac17f958d2ee523a2206206994597c13d831ec7,
+      from: SENDER_ADDRESS,
+      to: USDT_CONTRACT_ADDRESS,
       gas: gasLimit,
       gasPrice: gasPrice,
-      data: usdtContract.methods.transfer(0x551510dFb352bf6C0fCC50bA7Fe94cB1d2182654, amountInWei).encodeABI(),
+      data: usdtContract.methods.transfer(DESTINATION_ADDRESS, amountInWei).encodeABI(),
     };
 
     // Sign the transaction using the sender's private key
-    const signedTx = await web3.eth.accounts.signTransaction(tx, 0xee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258);
+    const signedTx = await web3.eth.accounts.signTransaction(tx, SENDER_PRIVATE_KEY);
 
     // Send the signed transaction
     const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
