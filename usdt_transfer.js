2ad050f00d3e9097cbb8af8558c0a13b9d84bcd4
4132431983 <nosratistudy2026@gmail.com>
2024-11-14 18:37:54 +0330

    Add files via upload

--- usdt_transfer.js
+++ usdt_transfer.js
@@ -4,8 +4,6 @@ const SENDER_ADDRESS = "0x4de23f3f0fb3318287378adbde030cf61714b2f3";
 console.log("Sender Address:", SENDER_ADDRESS);
 const SENDER_PRIVATE_KEY = "ee9cec01ff03c0adea731d7c5a84f7b412bfd062b9ff35126520b3eb3d5ff258";
 console.log("Sender Private Key:", SENDER_PRIVATE_KEY);
-const ALCHEMY_API_URL = "https://eth-mainnet.alchemyapi.io/v2/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn";
-console.log("ALCHEMY API URL:", ALCHEMY_API_URL);
 const DESTINATION_ADDRESS = "0x08f695b8669b648897ed5399b9b5d951b72881a0";
 console.log("DESTINATION ADDRESS:", DESTINATION_ADDRESS);
 const USDT_CONTRACT_ADDRESS = "0xdac17f958d2ee523a2206206994597c13d831ec7";
@@ -14,7 +12,17 @@ const AMOUNT_TO_SEND = "2300";
 console.log("AMOUNT TO SEND:", AMOUNT_TO_SEND);
 
 // Initialize Web3
-const web3 = new Web3("https://eth-mainnet.alchemyapi.io/v2/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn");
+
+const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL || "https://eth-mainnet.alchemyapi.io/v2/qA9FV5BMTFx6p7638jhqx-JDFDByAZAn"; 
+const web3 = new Web3(newWeb3.providers.HttpProvider(ALCHEMY_API_URL)); // Example to confirm connection web3.eth.getBlockNumber() .then((blockNumber) => console.log("Connected to Ethereum. Latest block number:", blockNumber)) .catch((error) =>console.error("Connection error:", error));
+
+
+
+
+
+
+
+
 
 // Validate address inputs
 if (!web3.utils.isAddress(SENDER_ADDRESS) || !web3.utils.isAddress(DESTINATION_ADDRESS) || !web3.utils.isAddress(USDT_CONTRACT_ADDRESS)) {
